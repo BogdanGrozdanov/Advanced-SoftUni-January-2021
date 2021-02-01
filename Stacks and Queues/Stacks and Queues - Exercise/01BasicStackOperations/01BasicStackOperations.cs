5177df1592d1b7c16dfedf16e8875291269b1987
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-02-01 15:54:47 +0000

    пробвам си нещо

---
+++ Stacks and Queues/Stacks and Queues - Exercise/01BasicStackOperations/01BasicStackOperations.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01BasicStackOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nSX = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int[] input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            Stack<int> stack = new Stack<int>(input);
+
+            int remove = nSX[1];
+            for (int i = 0; i < remove; i++)
+            {
+                stack.Pop();
+            }
+            if (stack.Count == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else
+            {
+                if (stack.Contains(nSX[2]))
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    Console.WriteLine(stack.Min());
+                }
+            }
+        }
+    }
+}
