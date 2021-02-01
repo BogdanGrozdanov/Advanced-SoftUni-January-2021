46bf458f4d77090f6e3005a6a40e2eaeec134c70
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-02-01 16:55:01 +0000

    Create 07TruckTour.cs

---
+++ 01.Stacks and Queues/Exercise Stacks and Queues/07TruckTour.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07TruckTour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfPumps = int.Parse(Console.ReadLine());
+            Queue<string> circle = new Queue<string>();
+
+            for (int i = 0; i < numberOfPumps; i++)
+            {
+                string input = Console.ReadLine();
+                input += $" {i}";
+                circle.Enqueue(input);
+            }
+            int totalFuel = 0;
+            for (int i = 0; i < numberOfPumps; i++)
+            {
+                string currentInfo = circle.Dequeue();
+                var info = currentInfo.Split().Select(int.Parse).ToArray();
+
+                var fuel = info[0];
+                var distance = info[1];
+                totalFuel += fuel;
+
+                if (totalFuel >= distance)
+                {
+                    totalFuel -= distance;
+                }
+                else
+                {
+                    totalFuel = 0;
+                    i--;
+                }
+                circle.Enqueue(currentInfo);
+            }
+        }
+
+    }
+}
