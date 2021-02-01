c5e25097da98cf85ff88620d8b5708066721d6af
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-02-01 16:54:55 +0000

    Create 04FastFood.cs

---
+++ 01.Stacks and Queues/Exercise Stacks and Queues/04FastFood.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04FastFood
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int allQuantity = int.Parse(Console.ReadLine());
+            int[] allOrders = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Queue<int> orders = new Queue<int>(allOrders);
+            Console.WriteLine(orders.Max());
+
+            while (orders.Count > 0)
+            {
+                int currOrderQuantity = orders.Peek();
+
+                if (currOrderQuantity > allQuantity)
+                {
+                    break;
+                }
+                else
+                {
+                    allQuantity -= orders.Dequeue();
+                }
+            }
+            if (orders.Count == 0)
+            {
+                Console.WriteLine("Orders complete");
+            }
+            if (orders.Count > 0)
+            {
+                Console.WriteLine("Orders left: " + string.Join(" ", orders));
+            }
+
+        }
+    }
+}
