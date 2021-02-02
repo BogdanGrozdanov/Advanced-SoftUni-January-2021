8a9d5cf29b3c911e6bc74bc57648d2c69a68d944
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-02-02 17:43:32 +0000

    Create 09SimpleTextEditor.cs

---
+++ 01.Stacks and Queues/Exercise Stacks and Queues-Exercises/09SimpleTextEditor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+
+namespace _09SimpleTextEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numComands = int.Parse(Console.ReadLine());
+            StringBuilder text = new StringBuilder();
+
+            for (int i = 0; i < numComands; i++)
+            {
+                string[] comand = Console.ReadLine().Split();
+
+                if (comand[0] == "1")
+                {
+                    text.Append(comand[1]);
+                }
+                else if (comand[0] == "2")
+                {
+                    text.Remove((text.Length - int.Parse(comand[1])), text.Length);
+                }
+                else if (comand[0] == "3")
+                {
+                    Console.WriteLine(text[int.Parse(comand[1]) - 1]);
+                }
+            }
+
+        }
+    }
+}
