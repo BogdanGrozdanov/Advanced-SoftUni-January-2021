95360dc9f33b5e89e58bcbfecc1f9cbbe33837a0
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-02-07 14:53:56 +0000

    Create Person.cs

---
+++ Defining Classes/Defining Classes - Exercise/03. Oldest Family Member/Person.cs
@@ -0,0 +1,38 @@
+﻿namespace DefiningClasses
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public Person()
+        {
+            this.Name = "No name";
+            this.Age = 1;
+        }
+
+        public Person(int age)
+            : this()
+        {
+            Age = age;
+        }
+
+        public Person(string name, int age):this()
+        {
+            Name = name;
+            Age = age;
+        }
+        
+
+        public string Name
+        {
+            get { return name; }
+            set { this.name = value; }
+        }
+        public int Age
+        {
+            get { return age; }
+            set { this.age = value; }
+        }
+    }
+}
