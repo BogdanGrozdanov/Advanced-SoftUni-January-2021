ff2f5dcdc96d09fe66a2665d74f1e0517e679639
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-02-07 13:07:14 +0000

    Create 1.DefineAClassPerson.cs

---
+++ Defining Classes/Defining Classes - Exercise/01. Define a Class Person/1.DefineAClassPerson.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person personOne = new Person()
+            {
+                Name = "Pesho",
+                Age = 20
+            };
+
+            Person personTwo = new Person()
+            {
+                Name = "Gosho",
+                Age = 18
+            };
+
+            Person personThree = new Person()
+            {
+                Name = "Stamat",
+                Age = 43
+            };
+
+        }
+    }
+}
