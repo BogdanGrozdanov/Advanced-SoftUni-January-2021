fd962472b42a3fdc73b3c0eef7c2fda3fd0938d9
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-02-07 22:34:27 +0000

    Create 05.DateModifier.cs

---
+++ Defining Classes/Defining Classes - Exercise/05.DateModifier/05.DateModifier.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DateModifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string first = Console.ReadLine();
+            string second = Console.ReadLine();
+
+            int days = DateModifier.GetDiffBetweenTwoDates(first, second);
+
+            Console.WriteLine(Math.Abs(days));
+        }
+    }
+}
