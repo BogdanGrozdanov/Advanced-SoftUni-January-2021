18743f2fcb488fec2b88d2dc17925f35fa93bb5e
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-02-07 17:35:19 +0000

    Create Family.cs

---
+++ Defining Classes/Defining Classes - Exercise/04.OpinionPoll/Family.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    class Family
+    {
+        public Family()
+        { this.Members = new List<Person>(); }
+
+
+        public List<Person> Members { get; set; }
+
+        public void AddMember(Person person)
+        {
+            this.Members.Add(person);
+        }
+
+        public Person GetGetOldestMember()
+        {
+            Person person = this.Members.OrderByDescending(p => p.Age).FirstOrDefault();
+            return person;
+
+        }
+    }
+}
