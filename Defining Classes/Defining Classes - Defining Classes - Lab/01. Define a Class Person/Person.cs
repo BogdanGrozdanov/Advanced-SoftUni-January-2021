91e505cde6318bce48d426ae89083d0aa2d3ca26
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-02-07 12:22:07 +0000

    Create Person.cs

---
+++ Defining Classes/Defining Classes - Defining Classes - Lab/01. Define a Class Person/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public string Name
+        {
+            get { return name; }
+            set { this.name = value; }
+        }
+        public int Age
+        {
+            get { return age; }
+            set { this.age = value; }
+        }
+    }
+}
