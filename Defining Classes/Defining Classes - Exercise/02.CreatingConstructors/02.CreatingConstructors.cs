4d59a4f1aaa8b611740d32d39120219a7d959ff9
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-02-07 13:07:24 +0000

    Create 02.CreatingConstructors.cs

---
+++ Defining Classes/Defining Classes - Exercise/02.CreatingConstructors/02.CreatingConstructors.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person first = new Person();
+            Person second = new Person(7);
+            Person thirth = new Person("Simona",7);
+            Console.WriteLine();
+        }
+
+    }
+}
