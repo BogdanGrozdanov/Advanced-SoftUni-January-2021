5533e22b0d16fcf8851a54c047ca7741f5f828a4
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-02-07 22:34:56 +0000

    Create DateModifier.cs

---
+++ Defining Classes/Defining Classes - Exercise/05.DateModifier/DateModifier.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DateModifier
+{
+    public static class DateModifier
+    {
+        public static int GetDiffBetweenTwoDates(string dateOneStr, string dateTwoStr)
+        {
+            DateTime dateOne = DateTime.Parse(dateOneStr);
+
+            DateTime dateTwo = DateTime.Parse(dateTwoStr);
+
+            TimeSpan diff = dateOne - dateTwo;
+            return diff.Days;
+        }
+    }
+}
