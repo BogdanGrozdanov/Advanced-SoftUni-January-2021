e56e755128ba7c06e2e48bbbb439b6babdd1014e
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-02-08 22:50:06 +0000

    Create Engine.cs

---
+++ Defining Classes/Defining Classes - Exercise/7.RawData/Engine.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RawData
+{
+    public class Engine
+    {
+        public Engine()
+        {
+            EngineSpeed = 0;
+            EnginePower = 0;
+        }
+        public Engine(int egineSpeed, int enginePower)
+        {
+            EngineSpeed = egineSpeed;
+            EnginePower = enginePower;
+        }
+
+        public int EngineSpeed { get; set; }
+
+        public int EnginePower { get; set; }
+
+    }
+}
