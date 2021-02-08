1aefd2fb60280a621956c4aa93758708ae92d41c
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-02-08 22:50:53 +0000

    Create Tire.cs

---
+++ Defining Classes/Defining Classes - Exercise/7.RawData/Tire.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace RawData
+{
+    public class Tire
+    {
+        public Tire()
+        {
+            TirePressure = 0;
+            TireAge = 0;
+        }
+        public Tire(double tirePressure, double tireAge)
+        {
+            TirePressure = tirePressure;
+            TireAge = tireAge;
+        }
+
+        public double TirePressure { get; set; }
+        public double TireAge { get; set; }
+        List<Tire> Tires = new List<Tire>();
+
+
+    }
+}
