f2ac538b7511f7d06cf90e679249cb8e97cde350
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-02-08 22:50:09 +0000

    Create Car.cs

---
+++ Defining Classes/Defining Classes - Exercise/7.RawData/Car.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace RawData
+{
+    public class Car
+    {
+        public Car()
+        {
+            
+        }
+
+        public Car(string model, Engine engine, Cargo cargo, List<Tire> tires)
+        {
+            Model = model;
+            Engine = engine;
+            Cargo = cargo;
+            tires = new List<Tire>();
+
+        }
+
+        public string Model { get; set; }
+        public Engine Engine { get; set; }
+        public Cargo Cargo { get; set; }
+        public List<Tire> Tires { get; set; }
+
+    }
+
+}
