50e4638e5a824dce0c8ca9684173ccc773dcee46
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-02-08 13:30:04 +0000

    Create Car.cs

---
+++ Defining Classes/Defining Classes - Exercise/06.SpeedRacing/Car.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace SpeedRacing
+{
+    public class Car
+    {
+        public Car()
+        {
+            Model = String.Empty;
+            FuelAmount = 0;
+            FuelConsumptionPerKilometer = 0;
+        }
+
+        public Car(string model, double fuelAmount, double fuelConsumptionFor1km) : this()
+        {
+            Model = model;
+            FuelAmount = fuelAmount;
+            FuelConsumptionPerKilometer = fuelConsumptionFor1km;
+        }
+
+        public string Model { get; set; }
+
+        public double FuelAmount { get; set; }
+
+        public double FuelConsumptionPerKilometer { get; set; }
+
+        public double TravelledDistance { get; set; }
+
+        public void Drive(double amountOfKm)
+        {
+            double fuelNeeded = FuelConsumptionPerKilometer * amountOfKm;
+
+            if (FuelAmount >= fuelNeeded)
+            {
+                FuelAmount -= fuelNeeded;
+                TravelledDistance += amountOfKm;
+            }
+            else
+            {
+                Console.WriteLine("Insufficient fuel for the drive");
+            }
+        }
+
+
+
+
+
+    }
+}
