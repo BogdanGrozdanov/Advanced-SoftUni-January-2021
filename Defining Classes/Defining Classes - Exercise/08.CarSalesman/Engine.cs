fdaa58f8abf82e99d60756e9dabd13099686c2fc
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-02-10 11:17:53 +0000

    Create Engine.cs

---
+++ Defining Classes/Defining Classes - Exercise/08.CarSalesman/Engine.cs
@@ -0,0 +1,41 @@
+﻿namespace _08.CarSalesman
+{
+    public class Engine
+    {
+        public Engine()
+        {
+
+        }
+        public void AddEngine(string[] paramEngine)
+        {
+            Engine engine = new Engine();
+
+            if (paramEngine.Length == 2)
+            {
+                Model = paramEngine[0];
+                Power = paramEngine[1];
+                Displacement = "n/a";
+                Efficiency = "n/a";
+            }
+            if (paramEngine.Length == 3)
+            {
+                Model = paramEngine[0];
+                Power = paramEngine[1];
+                Displacement = paramEngine[2];
+                Efficiency = "n/a";
+            }
+            if (paramEngine.Length == 4)
+            {
+                Model = paramEngine[0];
+                Power = paramEngine[1];
+                Displacement = paramEngine[2];
+                Efficiency = paramEngine[3];
+            }
+        }
+
+        public string Model { get; set; }
+        public string Power { get; set; }
+        public string Displacement { get; set; }
+        public string Efficiency { get; set; }
+    }
+}
