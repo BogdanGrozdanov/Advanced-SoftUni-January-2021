a305f99d9e9aec7432b0fd50dd791807490525fe
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-02-11 16:23:47 +0000

    Create Parking.cs

---
+++ Defining Classes/Defining Classes - Exercise/10.SoftUniParking/Parking.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SoftUniParking
+{
+    public class Parking
+    {
+        public Parking(int capacity)
+        {
+            this.Capacity = capacity;
+            cars = new List<Car>();
+        }
+
+        public int Count { get => cars.Count; }
+        public string AddCar(Car car)
+        {
+            if (cars.Any(c => c.RegistrationNumber == car.RegistrationNumber))
+            {
+                return "Car with that registration number, already exists!";
+            }
+            if (cars.Capacity == Capacity)
+            {
+                return "Parking is full!";
+            }
+            cars.Add(car);
+            return $"Successfully added new car {car.Make} {car.RegistrationNumber}";
+        }
+
+        public string RemoveCar(string registrationNumber)
+        {
+            Car car = cars.FirstOrDefault(c => c.RegistrationNumber == registrationNumber);
+            if (car == null)
+            {
+                return "Car with that registration number, doesn't exist!";
+            }
+            cars.Remove(car);
+            return $"Successfully removed {registrationNumber}";
+        }
+
+        public Car GetCar(string registrationNumber)
+        {
+            Car car = cars.FirstOrDefault(c => c.RegistrationNumber == registrationNumber);
+            return car;
+        }
+
+        public void RemoveSetOfRegistrationNumber(List<string> registrationNumbers)
+        {
+            cars = cars.Where(c => !registrationNumbers.Contains(c.RegistrationNumber)).ToList();
+        }
+
+        private List<Car> cars;
+        private int Capacity;
+    }
+}
