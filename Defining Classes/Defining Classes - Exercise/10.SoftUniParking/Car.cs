3dd8d968c23f731bfb344c2ecf2dcf415c1f1a78
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-02-11 16:23:45 +0000

    Create Car.cs

---
+++ Defining Classes/Defining Classes - Exercise/10.SoftUniParking/Car.cs
@@ -0,0 +1,25 @@
+﻿namespace SoftUniParking
+{
+    public class Car
+    {
+        public Car(string make, string model, int horsePower, string registrationNnumber)
+        {
+            Make = make;
+            Model = model;
+            HorsePower = horsePower;
+            RegistrationNumber = registrationNnumber;
+        }
+
+
+        public override string ToString()
+        {
+            return $"Make: {Make}\nModel: {Model}\nHorsePower: {HorsePower}\nRegistrationNumber: { RegistrationNumber}";
+        }
+
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int HorsePower { get; set; }
+        public string RegistrationNumber { get; set; }
+    }
+}
