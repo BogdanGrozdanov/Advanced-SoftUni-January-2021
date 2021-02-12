84474a551ee300fd07ad273d3dff991b634105c4
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-02-12 20:59:13 +0000

    Create Trainer.cs

---
+++ Defining Classes/Defining Classes - Exercise/09.PokemonTrainer/Trainer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PokemonTrainer
+{
+    public class Trainer
+    {
+        public Trainer()
+        {
+
+        }
+        public Trainer(string[] inputData) : this()
+        {
+            Name = inputData[0];
+            NumberOfBadges = defaultNumberOfBadges;
+            Pokemons = new List<Pokemon>();
+        }
+        public override string ToString()
+        {
+            return $"{Name} {NumberOfBadges} {Pokemons.Count}";
+        }
+        private string name;
+        private int numberOfBadges;
+        private List<Pokemon> pokemons;
+        private const int defaultNumberOfBadges = 0;
+
+
+        public string Name { get; set; }
+        public int NumberOfBadges { get; set; }
+        public List<Pokemon> Pokemons { get; set; }
+    }
+}
