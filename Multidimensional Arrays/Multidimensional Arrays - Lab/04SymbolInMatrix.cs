a44bf993f9e193e28191c07483b76294ce6d8d16
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-02-15 18:45:13 +0000

    Create 04SymbolInMatrix.cs

---
+++ Multidimensional Arrays/Multidimensional Arrays - Lab/04SymbolInMatrix.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace _04SymbolInMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int matrixSize = int.Parse(Console.ReadLine());
+            char[,] matrix = new char[matrixSize, matrixSize];
+
+            for (int row = 0; row < matrixSize; row++)
+            {
+                string rowData = Console.ReadLine();
+
+                for (int col = 0; col < matrixSize; col++)
+                {
+                    matrix[row, col] = rowData[col];
+                }
+            }
+            char symbol = char.Parse(Console.ReadLine());
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (symbol == matrix[row, col])
+                    {
+                        Console.WriteLine($"({row}, {col})");
+                        return;
+                    }
+                }
+            }
+            Console.WriteLine($"{symbol} does not occur in the matrix");
+        }
+    }
+}
