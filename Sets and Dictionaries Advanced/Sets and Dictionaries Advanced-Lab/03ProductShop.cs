787e7dc3ffb25ab0e7d179651aca6080c6973609
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-02-15 18:59:24 +0000

    Create 03ProductShop.cs

---
+++ Sets and Dictionaries Advanced/Sets and Dictionaries Advanced-Lab/03ProductShop.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03ProductShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            SortedDictionary<string, Dictionary<string, double>> output = new SortedDictionary<string, Dictionary<string, double>>();
+
+            while (input != "Revision")
+            {
+                var splitted = input.Split(",");
+                string supermarket = splitted[0];
+                string product = splitted[1];
+                double price = double.Parse(splitted[2]);
+                if (!output.ContainsKey(supermarket))
+                {
+                    output.Add(supermarket, new Dictionary<string, double>());
+                }
+
+                if (!output[supermarket].ContainsKey(product))
+                {
+                    output[supermarket].Add(product, price);
+                }
+
+                input = Console.ReadLine();
+            }
+            foreach (var shop in output)
+            {
+                Console.WriteLine($"{shop.Key}->");
+                foreach (var product in shop.Value)
+                {
+                    Console.WriteLine($"Product:{product.Key}, Price: {product.Value}");
+                }
+            }
+        }
+    }
+}
