7b75da1d8351ba702814303bd108e84060fa7a2c
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-02-15 18:59:30 +0000

    Create 06ParkingLot.cs

---
+++ Sets and Dictionaries Advanced/Sets and Dictionaries Advanced-Lab/06ParkingLot.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06ParkingLot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            HashSet<string> parkedCar = new HashSet<string>();
+
+            while (input != "END")
+            {
+                string[] splitted = input.Split(", ", StringSplitOptions.RemoveEmptyEntries);
+                string status = splitted[0];
+                string numPlate = splitted[1];
+                if (status == "IN")
+                {
+                    parkedCar.Add(numPlate);
+                }
+                if (status == "OUT")
+                {
+                    parkedCar.Remove(numPlate);
+                }
+                input = Console.ReadLine();
+            }
+            if (parkedCar.Count > 0)
+            {
+                foreach (var car in parkedCar)
+                {
+                    Console.WriteLine(car);
+                }
+            }
+            else if (parkedCar.Count <= 0)
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+
+
+        }
+    }
+}
