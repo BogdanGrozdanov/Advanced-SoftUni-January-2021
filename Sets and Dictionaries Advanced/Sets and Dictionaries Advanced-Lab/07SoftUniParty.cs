d3ef68ee7e5b0586def26cb5cf4fa93b190f1a24
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-02-15 18:59:33 +0000

    Create 07SoftUniParty.cs

---
+++ Sets and Dictionaries Advanced/Sets and Dictionaries Advanced-Lab/07SoftUniParty.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07SoftUniParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            HashSet<string> normal = new HashSet<string>();
+            HashSet<string> vips = new HashSet<string>();
+            while (input != "PARTY")
+            {
+                bool vip = char.IsDigit(input[0]);
+                if (vip)
+                {
+                    vips.Add(input);
+                }
+                else
+                {
+                    normal.Add(input);
+                }
+                input = Console.ReadLine();
+            }
+            while (input != "END")
+            {
+                if (vips.Contains(input))
+                {
+                    vips.Remove(input);
+                }
+                normal.Remove(input);
+                input = Console.ReadLine();
+            }
+            Console.WriteLine(normal.Count + vips.Count);
+            foreach (var guest in vips)
+            {
+                Console.WriteLine(guest);
+            }
+            foreach (var guest in normal)
+            {
+                Console.WriteLine(guest);
+            }
+
+
+        }
+    }
+}
