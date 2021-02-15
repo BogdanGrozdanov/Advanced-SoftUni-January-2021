7d3b6a9512e0d5769f11b7ffa1b01594dae2d64e
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-02-15 18:59:20 +0000

    Create 02AverageStudentGrades.cs

---
+++ Sets and Dictionaries Advanced/Sets and Dictionaries Advanced-Lab/02AverageStudentGrades.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02AverageStudentGrades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<string, List<decimal>> students = new Dictionary<string, List<decimal>>();
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine().Split();
+                string name = input[0];
+                decimal grade = decimal.Parse(input[1]);
+                if (!students.ContainsKey(name))
+                {
+                    students.Add(name, new List<decimal>());
+                }
+                students[name].Add(grade);
+            }
+            foreach (var student in students)
+            {
+                Console.Write($"{student.Key} -> ");
+                foreach (var grade in student.Value)
+                {
+                    Console.Write($"{grade:F2} ");
+                }
+                Console.WriteLine($"(avg: {student.Value.Average():f2})");
+            }
+        }
+    }
+}
