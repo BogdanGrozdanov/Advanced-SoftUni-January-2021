d8b227fabc380aff46d636430ae5d90d668be733
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-02-15 18:59:27 +0000

    Create 04CitiesByContinentAndCountry.cs

---
+++ Sets and Dictionaries Advanced/Sets and Dictionaries Advanced-Lab/04CitiesByContinentAndCountry.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04CitiesByContinentAndCountry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> names = new HashSet<string>();
+            int count = int.Parse(Console.ReadLine());
+            for (int i = 0; i < count; i++)
+            {
+                string input = Console.ReadLine();
+                names.Add(input);
+            }
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
