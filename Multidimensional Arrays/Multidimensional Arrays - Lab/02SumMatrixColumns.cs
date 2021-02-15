d569c6f7e71064c8b37899481b959d281a11f142
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-02-15 18:45:09 +0000

    Create 02SumMatrixColumns.cs

---
+++ Multidimensional Arrays/Multidimensional Arrays - Lab/02SumMatrixColumns.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace _02SumMatrixColumns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            int rows = input[0];
+            int cols = input[1];
+            int[,] matrix = new int[rows, cols];
+
+
+            for (int row = 0; row < rows; row++)
+            {
+                int[] rowData = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = rowData[col];
+                }
+            }
+            for (int col = 0; col < cols; col++)
+            {
+                int sumCol = 0;
+
+                for (int row = 0; row < rows; row++)
+                {
+                    sumCol += matrix[row, col];
+                }
+                Console.WriteLine(sumCol);
+
+            }
+
+        }
+    }
+}
